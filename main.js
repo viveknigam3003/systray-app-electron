7c003c7ad70eaa8a66f491c34cbdfe79c6bbb7b0
viveknigam3003 <viveknigam.nigam3@gmail.com>
2020-03-20 08:49:31 +0530

    First Commit

---
+++ main.js
@@ -0,0 +1,69 @@
+const {app, BrowserWindow, ipcMain, Tray} = require('electron');
+const path = require('path');
+
+let tray = undefined
+let window = undefined
+
+
+app.on('ready', () => {
+  createTray()
+  createWindow()
+})
+
+const createTray = () => {
+  tray = new Tray(path.join('electorn-logo-2.png'))
+  tray.on('click', function (event) {
+    toggleWindow()
+  });
+}
+
+const getWindowPosition = () => {
+  const windowBounds = window.getBounds();
+  const trayBounds = tray.getBounds();
+
+  // Center window horizontally below the tray icon
+  const x = Math.round(trayBounds.x - (trayBounds.width / 2) + (windowBounds.width / 2) - 250);
+
+  // Position window 4 pixels vertically below the tray icon
+  const y = Math.round(trayBounds.y - trayBounds.height - 415);
+
+  return {x: x, y: y};
+}
+
+const createWindow = () => {
+  window = new BrowserWindow({
+    width: 320,
+    height: 450,
+    show: false,
+    frame: false,
+    fullscreenable: false,
+    resizable: false,
+    transparent: false,
+    skipTaskbar: true,
+    webPreferences: {
+      backgroundThrottling: false
+    }
+  })
+  window.loadURL(`file://${path.join(__dirname, 'index.html')}`)
+
+  // Hide the window when it loses focus
+  window.on('blur', () => {
+    if (!window.webContents.isDevToolsOpened()) {
+      window.hide()
+    }
+  })
+}
+
+const toggleWindow = () => {
+  window.isVisible() ? window.hide() : showWindow();
+}
+
+const showWindow = () => {
+  const position = getWindowPosition();
+  window.setPosition(position.x, position.y, false);
+  window.show();
+}
+
+ipcMain.on('show-window', () => {
+  showWindow()
+})
